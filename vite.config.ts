c91da4c6c47880e0c42cf0998bb0326495ba168c
esteban-vm <estebanvm90@outlook.com>
2023-12-11 09:23:11 -0300

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,13 @@
 import { defineConfig, type CommonServerOptions } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { VitePWA } from 'vite-plugin-pwa'
 
 const commonOptions: CommonServerOptions = { open: true, host: true }
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: { target: 'ESNext' },
-  plugins: [react(), tsconfigPaths(), VitePWA({})],
+  plugins: [react(), tsconfigPaths()],
   server: { ...commonOptions, port: 5_174 },
   preview: { ...commonOptions, port: 5_175 },
 })
